8a40c88b9fe8f892c0f61c1ed511d94f0a91189e
qinchangjin <18039670302@163.com>
2019-07-19 14:10:09 +0800

    --git提交测试

---
+++ all/src/com.qin/Test2.java
@@ -0,0 +1,11 @@
+/**
+ * @author Administrator
+ * @date 2019-07-19, 14:08
+ */
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        System.out.println("Test2");
+    }
+}
